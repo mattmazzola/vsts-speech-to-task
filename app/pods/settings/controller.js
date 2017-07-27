0a31535a8af8ba7c8f33d59c78369830a21a0e06
Matt Mazzola <mattm@microsoft.com>
2017-07-26 20:47:19 -0700

    More cleanup of item creation with tag option and better styling

--- app/pods/settings/controller.js
+++ app/pods/settings/controller.js
@@ -5,17 +5,28 @@ export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   vsts: Ember.inject.service('vsts'),
 
+  init() {
+    this._super();
+
+    this.set('account', 'mattmazzola')
+    this.set('area', 'schultztables')
+    this.set('itemType', 'Task')
+    this.set('tag', 'VSTS-Speech-to-Task')
+  },
+
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
     },
-    
+
     createWorkItem() {
       this.get('vsts').createWorkItem({
+        account: this.get('account'),
         title: this.get('title'),
         description: this.get('description'),
         itemType: this.get('itemType'),
-        area: this.get('area')
+        area: this.get('area'),
+        tag: this.get('tag')
       })
     }
   }
--- app/services/vsts.js
+++ app/services/vsts.js
@@ -4,7 +4,38 @@ import fetch from 'fetch';
 export default Ember.Service.extend({
   session: Ember.inject.service('session'),
 
-  createWorkItem({ area, title, description, itemType }) {
-    console.log(area, title, description, itemType)
+  createWorkItem(itemData) {
+    console.log(itemData)
+
+    const { account, area, title, description, itemType, tag } = itemData
+    const accessToken = this.get('session.data.authenticated.access_token');
+
+    fetch(`https://${account}.visualstudio.com/DefaultCollection/${area}/_apis/wit/workitems/$${itemType}?api-version=1.0`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json-patch+json'
+      },
+      body: JSON.stringify([
+        {
+          "op": "add",
+          "path": "/fields/System.Title",
+          "value": title
+        },
+        {
+          "op": "add",
+          "path": "/fields/System.Tags",
+          "value": tag
+        }
+      ])
+    })
+      .then(response => {
+        return response.json().then(json => {
+          if (!response.ok) throw new Error(json.message || json.ErrorDescription)
+          console.log(json)
+          return json
+        })
+      })
+
   }
 });
