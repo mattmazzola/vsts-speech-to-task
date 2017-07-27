ba140612b3154b90cd95de551e7d6dcc2b571924
Matt Mazzola <mattm@microsoft.com>
2017-07-26 22:13:36 -0700

    Add ability to create which will catch all items created by app

--- app/pods/settings/controller.js
+++ app/pods/settings/controller.js
@@ -12,6 +12,7 @@ export default Ember.Controller.extend({
     this.set('area', 'schultztables')
     this.set('itemType', 'Task')
     this.set('tag', 'VSTS-Speech-to-Task')
+    this.set('queryName', 'VSTS-Speech-to-Task')
   },
 
   actions: {
@@ -26,6 +27,15 @@ export default Ember.Controller.extend({
         description: this.get('description'),
         itemType: this.get('itemType'),
         area: this.get('area'),
+        tag: this.get('tag')
+      })
+    },
+
+    createQuery() {
+      this.get('vsts').createQuery({
+        account: this.get('account'),
+        area: this.get('area'),
+        name: this.get('queryName'),
         tag: this.get('tag')
       })
     }
--- app/services/vsts.js
+++ app/services/vsts.js
@@ -36,6 +36,29 @@ export default Ember.Service.extend({
           return json
         })
       })
+  },
 
+  createQuery(queryData) {
+    const { account, area, name, tag } = queryData
+    const accessToken = this.get('session.data.authenticated.access_token');
+
+    fetch(`https://${account}.visualstudio.com/DefaultCollection/${area}/_apis/wit/queries/My%20Queries?api-version=1.0`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "name": name,
+        "wiql": `Select [System.Id], [System.WorkItemType], [System.Title], [System.State], [System.AreaPath], [System.IterationPath], [System.Tags] From WorkItems Where [System.Tags] Contains "${tag}" order by [Microsoft.VSTS.Common.Priority] asc, [System.CreatedDate] desc`
+      })
+    })
+      .then(response => {
+        return response.json().then(json => {
+          if (!response.ok) throw new Error(json.message || json.ErrorDescription || JSON.stringify(json))
+          console.log(json)
+          return json
+        })
+      })
   }
 });
