445ac547bf9684540c26009fd5297cca3ec32da8
Matt Mazzola <mattm@microsoft.com>
2017-07-28 11:51:05 -0700

    Update index page to leverage speech SDK to create items

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,11 +1,144 @@
 import Ember from 'ember';
+import * as speechSdk from 'Speech.Browser.Sdk';
+
+const {
+  inject
+} = Ember;
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
+  session: inject.service('session'),
+  vsts: inject.service('vsts'),
+
+  subscriptionKey: 'ecbc3f3d0c894cb088767321f4ae73f7',
+  isRecording: false,
+  hypothesis: '',
+  recognizer: null,
+
+  init() {
+    this._super()
+    const recognizer = this.setupRecognizer(
+      speechSdk,
+      speechSdk.RecognitionMode.Interactive,
+      "en-US",
+      speechSdk.SpeechResultFormat["Simple"],
+      this.get('subscriptionKey'))
+
+    this.set('recognizer', recognizer)
+  },
+
+  setupRecognizer(SDK, recognitionMode, language, format, subscriptionKey) {
+    var recognizerConfig = new SDK.RecognizerConfig(
+      new SDK.SpeechConfig(
+        new SDK.Context(
+          new SDK.OS(navigator.userAgent, "Browser", null),
+          new SDK.Device("VSTS-Speech-to-Task", "VSTS-Speech-to-Task", "1.0.00000"))),
+      recognitionMode,    // SDK.RecognitionMode.Interactive  (Options - Interactive/Conversation/Dictation>)
+      language,           // Supported laguages are specific to each recognition mode. Refer to docs.
+      format);            // SDK.SpeechResultFormat.Simple (Options - Simple/Detailed)
+
+    // Alternatively use SDK.CognitiveTokenAuthentication(fetchCallback, fetchOnExpiryCallback) for token auth
+    var authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
+
+    return SDK.CreateRecognizer(recognizerConfig, authentication);
+  },
+
+  updateRecognizedHypothesis(hypothesis) {
+    this.set('hypothesis', hypothesis)
+  },
+
+  updateRecognizedPhrase(phrase) {
+    console.log(phrase)
+  },
+
+  onSpeechEndDetected() {
+    this.set('isRecording', false)
+  },
+
+  onComplete() {
+    this.set('isRecording', false)
+  },
+
+  startRecording() {
+    console.log('start recording')
+    
+    this.set('isRecording', true)
+    const recognizer = this.get('recognizer')
+    recognizer.Recognize((event) => {
+      /*
+       Alternative syntax for typescript devs.
+       if (event instanceof SDK.RecognitionTriggeredEvent)
+      */
+      switch (event.Name) {
+        case "RecognitionTriggeredEvent":
+          // this.updateStatus("Initializing");
+          break;
+        case "ListeningStartedEvent":
+          // this.updateStatus("Listening");
+          break;
+        case "RecognitionStartedEvent":
+          // this.updateStatus("Listening_Recognizing");
+          break;
+        case "SpeechStartDetectedEvent":
+          // this.updateStatus("Listening_DetectedSpeech_Recognizing");
+          break;
+        case "SpeechHypothesisEvent":
+          this.updateRecognizedHypothesis(event.Result.Text);
+          break;
+        case "SpeechEndDetectedEvent":
+          this.onSpeechEndDetected();
+          break;
+        case "SpeechSimplePhraseEvent":
+          this.updateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+          break;
+        case "SpeechDetailedPhraseEvent":
+          this.updateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+          break;
+        case "RecognitionEndedEvent":
+          this.onComplete();
+          console.log(JSON.stringify(event)); // Debug information
+          break;
+      }
+    })
+      .On(() => {
+        // The request succeeded. Nothing to do here.
+      },
+      (error) => {
+        console.error(error);
+      });
+  },
+
+  stopRecording() {
+    const recognizer = this.get('recognizer')
+    recognizer.AudioSource.TurnOff();
+    this.onComplete();
+  },
 
   actions: {
     record() {
-      console.log('startrecording')
+      const isRecording = this.get('isRecording')
+
+      if (isRecording) {
+        this.stopRecording()
+      }
+      else {
+        this.startRecording()
+      }
+    },
+
+    confirm() {
+      const vsts = this.get('vsts')
+
+      vsts.createWorkItem({
+        account: "mattmazzola",
+        area: "schultztables",
+        itemType: "Task",
+        tag: "tag",
+        title: this.get('hypothesis'),
+        description: this.get('hypothesis')
+      })
+        .then(() => {
+          this.set('hypothesis', '')
+        })
     }
   }
 });
