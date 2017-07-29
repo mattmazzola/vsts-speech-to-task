34d35a13609f332f32ea6f03ff7fbbb095cac3e6
Matt Mazzola <mattmazzola@live.com>
2017-07-29 12:57:29 -0700

    Consolidate fetch code to helpers

--- app/services/vsts.js
+++ app/services/vsts.js
@@ -83,30 +83,12 @@ export default Ember.Service.extend({
   },
 
   findProjects(accountName) {
-    const accessToken = this.get('session.data.authenticated.access_token');
-
-    return fetch(`https://${accountName}.visualstudio.com/DefaultCollection/_apis/projects?api-version=1.0`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Accept': 'application/json'
-      }
-    })
-      .then(r => this.handleResponse(r))
+    return this.getJson(`https://${accountName}.visualstudio.com/DefaultCollection/_apis/projects?api-version=1.0`)
       .then(json => json.value)
   },
 
   findWorkItemTypes(accountName, project) {
-    const accessToken = this.get('session.data.authenticated.access_token');
-
-    return fetch(`https://${accountName}.visualstudio.com/DefaultCollection/${project}/_apis/wit/workItemTypes?api-version=1.0`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Accept': 'application/json'
-      }
-    })
-      .then(r => this.handleResponse(r))
+    return this.getJson(`https://${accountName}.visualstudio.com/DefaultCollection/${project}/_apis/wit/workItemTypes?api-version=1.0`)
       .then(json => json.value)
   },
 
@@ -125,31 +107,23 @@ export default Ember.Service.extend({
 
     const accessToken = this.get('session.data.authenticated.access_token');
 
-    return fetch(`https://${account}.visualstudio.com/DefaultCollection/${project}/_apis/wit/workitems/$${itemType}?api-version=1.0`, {
-      method: 'PATCH',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json-patch+json'
+    return this.patchJson(`https://${account}.visualstudio.com/DefaultCollection/${project}/_apis/wit/workitems/$${itemType}?api-version=1.0`, [
+      {
+        "op": "add",
+        "path": "/fields/System.Title",
+        "value": title
       },
-      body: JSON.stringify([
-        {
-          "op": "add",
-          "path": "/fields/System.Title",
-          "value": title
-        },
-        {
-          "op": "add",
-          "path": "/fields/System.Description",
-          "value": description
-        },
-        {
-          "op": "add",
-          "path": "/fields/System.Tags",
-          "value": tag
-        }
-      ])
-    })
-      .then(r => this.handleResponse(r))
+      {
+        "op": "add",
+        "path": "/fields/System.Description",
+        "value": description
+      },
+      {
+        "op": "add",
+        "path": "/fields/System.Tags",
+        "value": tag
+      }
+    ])
   },
 
   createQuery(queryData) {
@@ -164,18 +138,49 @@ export default Ember.Service.extend({
     console.log(`createQuery merged: ${JSON.stringify(merged, null, '  ')}`)
     const { account, project, name, tag } = merged
 
+    return this.postJson(`https://${account}.visualstudio.com/DefaultCollection/${project}/_apis/wit/queries/My%20Queries?api-version=1.0`, {
+      "name": name,
+      "wiql": `Select [System.Id], [System.WorkItemType], [System.Title], [System.State], [System.AreaPath], [System.IterationPath], [System.Tags] From WorkItems Where [System.Tags] Contains "${tag}" order by [Microsoft.VSTS.Common.Priority] asc, [System.CreatedDate] desc`
+    })
+  },
+
+  getJson(url) {
     const accessToken = this.get('session.data.authenticated.access_token');
 
-    return fetch(`https://${account}.visualstudio.com/DefaultCollection/${project}/_apis/wit/queries/My%20Queries?api-version=1.0`, {
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Accept': 'application/json'
+      }
+    })
+      .then(r => this.handleResponse(r))
+  },
+
+  patchJson(url, body) {
+    const accessToken = this.get('session.data.authenticated.access_token');
+
+    return fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json-patch+json'
+      },
+      body: JSON.stringify(body)
+    })
+      .then(r => this.handleResponse(r))
+  },
+
+  postJson(url, body) {
+    const accessToken = this.get('session.data.authenticated.access_token');
+
+    return fetch(url, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        "name": name,
-        "wiql": `Select [System.Id], [System.WorkItemType], [System.Title], [System.State], [System.AreaPath], [System.IterationPath], [System.Tags] From WorkItems Where [System.Tags] Contains "${tag}" order by [Microsoft.VSTS.Common.Priority] asc, [System.CreatedDate] desc`
-      })
+      body: JSON.stringify(body)
     })
       .then(r => this.handleResponse(r))
   },
